09b917d29bc271ba0f9e8e9991d124f499de3ce2
Christopher Siems <csiems@gmail.com>
2016-04-05 19:50:13 -0700

    add request permissions at scanner runtime

--- app/src/main/java/com/epicodus/librarius/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/librarius/ui/MainActivity.java
@@ -1,18 +1,24 @@
 package com.epicodus.librarius.ui;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 
 import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.epicodus.librarius.LibrariusApplication;
 import com.epicodus.librarius.fragments.BarcodeScannerFragment;
@@ -27,10 +33,12 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private static final int MY_PERMISSIONS_REQUEST_CAMERA = 123;
     @Bind(R.id.scanButton) FloatingActionButton mScanButton;
     @Bind(R.id.searchButton) FloatingActionButton mSearchButton;
     @Bind(R.id.manualEntryButton) FloatingActionButton mManualEntryButton;
     private Firebase mFirebaseRef;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -99,7 +107,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         if (v == mScanButton) {
-            launchBarcodeScannerFragment();
+            int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+            if (permissionCheck == PackageManager.PERMISSION_GRANTED) {
+                launchBarcodeScannerFragment();
+            } else {
+                showMessageOKCancel("You need to allow access to Contacts",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, MY_PERMISSIONS_REQUEST_CAMERA);
+                            }
+                        });
+                return;
+            }
         }
         if (v == mSearchButton) {
             launchSearchFragment();
@@ -107,6 +127,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (v == mManualEntryButton) {
             launchManualEntryFormFragment();
         }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_CAMERA: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(MainActivity.this, "Thank you!", Toast.LENGTH_SHORT)
+                            .show();
+                } else {
+                    Toast.makeText(MainActivity.this, "CAMERA Use Denied", Toast.LENGTH_SHORT)
+                            .show();
+                }
+                break;
+            }
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(MainActivity.this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
     }
 
     private void launchBarcodeScannerFragment() {
